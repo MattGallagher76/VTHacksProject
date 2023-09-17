d9ff02b97555aec4a18c789da45d74255425e137
cjmcdd <89484559+cjmcdd@users.noreply.github.com>
2023-09-17 03:38:26 -0400

    changing clothes

--- FittingRoom/Assets/ClothingSelection.cs
+++ FittingRoom/Assets/ClothingSelection.cs
@@ -12,14 +12,21 @@ public class ClothingSelection : MonoBehaviour
     void Start()
     {
         avatar = GetComponent<DynamicCharacterAvatar>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        avatar.SetSlot("Chest", "FemaleShirt1");
-        avatar.BuildCharacter();
-        avatar.SetSlot("Legs", "FemalePants1");
-        avatar.BuildCharacter();
+        int i = 0;
+        if(i == 0){
+             avatar.SetSlot("Chest", "FemaleShirt1");
+            avatar.BuildCharacter();
+            avatar.SetSlot("Legs", "FemalePants1");
+            avatar.BuildCharacter();
+            i++;
+
+        }
+       
     }
 }
--- FittingRoom/Assets/uiNavigation.cs
+++ FittingRoom/Assets/uiNavigation.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UMA;
+using UMA.CharacterSystem;
 public class uiNavigation : MonoBehaviour
 {
     public Canvas uiCanvas;
@@ -25,6 +27,10 @@ public class uiNavigation : MonoBehaviour
     public Canvas pantsColors;
     public Canvas shoeColors;
 
+      public Canvas shirtOptions;
+    public Canvas pantsOptions;
+    public Canvas shoeOptions;
+
     public Button activateMenuButton;
      public Button activateClothesMenuButton;
     public Button volumeOnButton;
@@ -35,12 +41,32 @@ public class uiNavigation : MonoBehaviour
     public GameObject music;   
     public GameObject woman;
    
+   private DynamicCharacterAvatar avatar;
+
+   private Color red;
+   private Color orange;
+   private Color yellow;
+   private Color green;
+   private Color blue;
+   private Color indigo;
+   private Color violet;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
         
+        avatar = woman.GetComponent<DynamicCharacterAvatar>();
+
+        red = new Color(0, 0.59f, 0.59f, 0.3f);
+        orange = new Color(0, 0.31f, 0.53f, 0.16f);
+        yellow = new Color(0, 0.19f, 0.69f, 0.12f);
+        green = new Color(0.33f, 0, 0.34f, 0.6f);
+        blue = new Color(0.59f, 0.33f, 0, 0.45f);
+        indigo = new Color(0.31f, 0, 0, 0.11f);
+        violet = new Color (0.21f, 0.73f, 0, 0);
+
     }
 
     // Update is called once per frame
@@ -194,30 +220,239 @@ public class uiNavigation : MonoBehaviour
     }
 
     public void shirtButtonClick(){
-        shirtColors.gameObject.SetActive(true);
+        shirtOptions.gameObject.SetActive(true);
     }
 
-    public void shirtColorClick(){
+    public void shirtColorRedClick(){
         shirtColors.gameObject.SetActive(false);
+        
+        avatar.SetColor("Shirt", red);
+        avatar.BuildCharacter();
     }
+    public void shirtColorOrangeClick(){
+        shirtColors.gameObject.SetActive(false);
+
+       avatar.SetColor("Shirt", orange);
+        avatar.BuildCharacter();
+    }
+    public void shirtColorYellowClick(){
+        shirtColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shirt", yellow);
+        avatar.BuildCharacter();
+
+        Debug.Log("we are in shirt yellow");
+    }
+    public void shirtColorGreenClick(){
+        shirtColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shirt", green);
+        avatar.BuildCharacter();
+    }
+    public void shirtColorBlueClick(){
+        shirtColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shirt", blue);
+        avatar.BuildCharacter();
+    }
+    public void shirtColorIndigoClick(){
+        shirtColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shirt", indigo);
+        avatar.BuildCharacter();
+    }
+    public void shirtColorVioletClick(){
+        shirtColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shirt", violet);
+        avatar.BuildCharacter();
+    }
+
+
 
     public void pantsButtonClick(){
-        pantsColors.gameObject.SetActive(true);
+        pantsOptions.gameObject.SetActive(true);
     }
 
-    public void pantsColorClick(){
+    public void pantsColorRedClick(){
         pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", red);
+        avatar.BuildCharacter();
     }
+    public void pantsColorOrangeClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", orange);
+        avatar.BuildCharacter();
+    }
+    public void pantsColorYellowClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", yellow);
+        avatar.BuildCharacter();
+    }
+    public void pantsColorGreenClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", green);
+        avatar.BuildCharacter();
+    }
+    public void pantsColorBlueClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", blue);
+        avatar.BuildCharacter();
+    }
+    public void pantsColorIndigoClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", indigo);
+        avatar.BuildCharacter();
+    }
+    public void pantsColorVioletClick(){
+        pantsColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Pants", violet);
+        avatar.BuildCharacter();
+    }
+
+
+
+
 
     public void shoeButtonClick(){
-        shoeColors.gameObject.SetActive(true);
+        shoeOptions.gameObject.SetActive(true);
+
+        
     }
 
-    public void shoeColorClick(){
+    public void shoeColorRedClick(){
         shoeColors.gameObject.SetActive(false);
+        
+        avatar.SetColor("Shoes", red);
+        avatar.BuildCharacter();
     }
+    public void shoeColorOrangeClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        
+        
+        avatar.SetColor("Shoes", orange);
+        avatar.BuildCharacter();
+    }
+    public void shoeColorYellowClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        
+        
+        avatar.SetColor("Shoes", yellow);
+        avatar.BuildCharacter();
+
+        
+    }
+    public void shoeColorGreenClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        
+        
+        avatar.SetColor("Shoes", green);
+        avatar.BuildCharacter();
+    }
+    public void shoeColorBlueClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        
+        avatar.SetColor("Shoes", blue);
+        avatar.BuildCharacter();
+    }
+    public void shoeColorIndigoClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        
+        avatar.SetColor("Shoes", indigo);
+        avatar.BuildCharacter();
+    }
+    public void shoeColorVioletClick(){
+        shoeColors.gameObject.SetActive(false);
+
+        avatar.SetColor("Shoes", violet);
+        avatar.BuildCharacter();
+    }
+
 
     public void makeWomanAppear(){
         woman.SetActive(true);
+    }
+
+
+    public void clickShirt1(){
+        shirtOptions.gameObject.SetActive(false);
+        shirtColors.gameObject.SetActive(true);
+
+        avatar.ClearSlot("Chest");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Chest", "FemaleShirt1");
+        avatar.BuildCharacter();
+
+    }
+    public void clickShirt2(){
+         shirtOptions.gameObject.SetActive(false);
+        shirtColors.gameObject.SetActive(true); 
+
+        avatar.ClearSlot("Chest");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Chest", "FemaleShirt2");
+        avatar.BuildCharacter();
+    }
+
+    public void clickShirt3(){
+         shirtOptions.gameObject.SetActive(false);
+        shirtColors.gameObject.SetActive(true); 
+
+        avatar.ClearSlot("Chest");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Chest", "FemaleShirt4");
+        avatar.BuildCharacter();
+    }
+
+    public void clickPants1(){
+        pantsOptions.gameObject.SetActive(false);
+        pantsColors.gameObject.SetActive(true); 
+
+        avatar.ClearSlot("Legs");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Legs", "FemalePants1");
+        avatar.BuildCharacter();
+    }
+
+    public void clickPants2(){
+        pantsOptions.gameObject.SetActive(false);
+        pantsColors.gameObject.SetActive(true); 
+
+        avatar.ClearSlot("Legs");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Legs", "FemalePants2");
+        avatar.BuildCharacter();
+    }
+
+
+    public void clickShoes1(){
+        shoeOptions.gameObject.SetActive(false);
+        shoeColors.gameObject.SetActive(true); 
+
+        avatar.ClearSlot("Feet");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Feet", "FemaleTallShoes_Black");
+        avatar.BuildCharacter();
+    }
+    public void clickShoes2(){
+        shoeOptions.gameObject.SetActive(false);
+        shoeColors.gameObject.SetActive(true);
+
+        avatar.ClearSlot("Feet");
+        avatar.BuildCharacter();
+        avatar.SetSlot("Feet", "FemaleTallShoes_White");
+        avatar.BuildCharacter(); 
     }
 }
