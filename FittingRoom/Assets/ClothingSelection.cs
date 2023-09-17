1f8d6d9f238a500f62af52251c873cf944700e23
LeahElana <leah.ican@gmail.com>
2023-09-16 21:39:11 -0400

    Clothing Selection Script

---
+++ FittingRoom/Assets/ClothingSelection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+
+public class ClothingSelection : MonoBehaviour
+{
+
+    private DynamicCharacterAvatar avatar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        avatar = GetComponent<DynamicCharacterAvatar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        avatar.SetSlot("Chest", "FemaleShirt1");
+        avatar.BuildCharacter();
+    }
+}
