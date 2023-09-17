a753a7a2f7d4be45522495e43eec8b323bbc79d2
MattGallagher76 <82038336+MattGallagher76@users.noreply.github.com>
2023-09-17 01:37:49 -0400

    Fixed mirror again and added hangers
    Fixed mirror again and added hangers

--- FittingRoom/Assets/MirrorMovement.cs
+++ FittingRoom/Assets/MirrorMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MirrorMovement : MonoBehaviour
 {
     public Transform playerTarget;
     public Transform mirror;
+    public float offset;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,12 @@ public class MirrorMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 camVec = playerTarget.position - mirror.position;
+        Vector3 tempPos = new Vector3(mirror.transform.position.x, playerTarget.transform.position.y, mirror.transform.position.z);
+
+        Vector3 camVec = playerTarget.position - tempPos;
         transform.localPosition = -1 * camVec;
         transform.LookAt(mirror);
+
+        GetComponent<Camera>().nearClipPlane = camVec.magnitude + offset;
     }
 }
